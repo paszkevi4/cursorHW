1a572ffa938d9326da930d5d956801c4bfae9695
paszkevi4 <paszkevi4@aol.com>
2020-08-05 20:53:56 +0300

    Updates

---
+++ precache-manifest.a2358c87f80e94549df24d2b7e25df9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d30ba40cbb9b695a79a06141da42aba",
+    "url": "/cursorHW/index.html"
+  },
+  {
+    "revision": "6b01e2091434f3de5d44",
+    "url": "/cursorHW/static/css/main.0e2b8bfc.chunk.css"
+  },
+  {
+    "revision": "725c547a7ec4a1358aaf",
+    "url": "/cursorHW/static/js/2.8560cb79.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/cursorHW/static/js/2.8560cb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b01e2091434f3de5d44",
+    "url": "/cursorHW/static/js/main.ca5b49bc.chunk.js"
+  },
+  {
+    "revision": "39d56f6a00684ce14420",
+    "url": "/cursorHW/static/js/runtime-main.66ebd486.js"
+  }
+]);
